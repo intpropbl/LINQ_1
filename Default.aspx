cad9e4cc7b776f523a7910a96d2ba30576c230b7
yazprogram <44929075+yazprogram@users.noreply.github.com>
2018-11-10 21:11:39 +0300

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <asp:ListBox ID="ListBox1" runat="server"></asp:ListBox>
+    <br />
+    <br />
+    Adı:&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="txtAd" runat="server"></asp:TextBox>
+    <br />
+    Soyadı:<asp:TextBox ID="txtSoyad" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" onclick="Button1_Click" Text="Ekle" />
+    <br />
+    <br />
+    <br />
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        LINQORNEKDataContext dc = new LINQORNEKDataContext();
+
+        var ogrenciler = from ogrenci in dc.OGRENCIs select ogrenci;
+
+
+        foreach (var tekkayit in ogrenciler)
+            ListBox1.Items.Add(tekkayit.AD.ToString()+" "+tekkayit.SOYAD.ToString());
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        LINQORNEKDataContext dc = new LINQORNEKDataContext();
+
+        OGRENCI ogr = new OGRENCI()
+        {
+            AD = txtAd.Text,
+            SOYAD = txtSoyad.Text
+        };
+
+        dc.OGRENCIs.InsertOnSubmit(ogr);
+        dc.SubmitChanges();
+    }
+}
